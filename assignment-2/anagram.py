3ef63141303e884edd579f1646f59b5a346bc441
Srinidhi J <srinidhj.21@gmail.com>
2013-03-17 21:52:09 +0530

    Assignment 2

--- assignment-2/anagram.py
+++
@@ -1,7 +0,0 @@
-def isAnagram(m,n):
-	mstr = ''.join(m.split())
-	nstr = ''.join(n.split())
-	mstr = ''.join(sorted(list(mstr)))
-	nstr = ''.join(sorted(list(nstr)))
-	return mstr == nstr
-print isAnagram('mother in law','woman hitler')
